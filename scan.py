a4704cd632e7a0d7ee411add09b102c9f8d26a5a
Kishiin <47154137+Tesselay@users.noreply.github.com>
2024-06-05 19:42:18 +0200

    Get all endpoints of tenant

--- scan.py
+++ scan.py
@@ -1,11 +1,5 @@
 import requests
 
-def main(id, secret):
-    auth_response = auth(id, secret)
-    tenant_response = getTenant(auth_response['access_token'])
-
-    data_region = tenant_response['apiHosts']['dataRegion']
-
 def auth(id, secret):
     payload = {'grant_type':'client_credentials', 'client_id':id, 'client_secret':secret, 'scope':'token'}
     headers = {'Content-Type':'application/x-www-form-urlencoded'}
@@ -18,6 +12,18 @@ def getTenant(jwt):
 
     response = requests.get('https://api.central.sophos.com/whoami/v1', headers=headers)
     return response
+
+def getAllEndpoints(jwt, id, data_region):
+    headers = {'Authorization: Bearer {}'.format(jwt), 'X-Tenant-ID: {}'.format(id)}
+
+    response = requests.get('{}/endpoint/v1/endpoints'.format(data_region))
+    return response
+
+def main(id, secret):
+    auth_response = auth(id, secret)
+    tenant_response = getTenant(auth_response['access_token'])
+
+    data_region = tenant_response['apiHosts']['dataRegion']
 
 if __name__ == '__main__':
     main()
