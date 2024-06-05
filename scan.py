2b972dd138658ff89c777a61340d68445cf85036
Kishiin <47154137+Tesselay@users.noreply.github.com>
2024-06-05 20:20:45 +0200

    Add json encoder to responses

--- scan.py
+++ scan.py
@@ -6,33 +6,33 @@ def auth(id, secret):
     headers = {'Content-Type':'application/x-www-form-urlencoded'}
 
     response = requests.post('https://id.sophos.com/api/v2/oauth2/token', params=payload, headers=headers)
-    return response
+    return response.json()
 
 def getTenant(jwt):
     headers = {'Authorization: Bearer {}'.format(jwt)}
 
     response = requests.get('https://api.central.sophos.com/whoami/v1', headers=headers)
-    return response
+    return response.json()
 
 def getAllEndpoints(jwt, id, data_region):
     headers = {'Authorization: Bearer {}'.format(jwt), 'X-Tenant-ID: {}'.format(id)}
 
     response = requests.get('{}/endpoint/v1/endpoints'.format(data_region), headers=headers)
-    endpoints = response['items']
-    while response['nextKey'] != '':
-        payload = {'pageFromKey': response['nextKey']}
+    endpoints = response.json()['items']
+    while response.json()['nextKey'] != '':
+        payload = {'pageFromKey': response.json()['nextKey']}
         response = requests.get('{}/endpoint/v1/endpoints'.format(data_region), headers=headers, params=payload)
-        endpoints.append(response['items'])
+        endpoints.append(response.json()['items'])
 
     return endpoints
 
 def main(id, secret):
     auth_response = auth(id, secret)
-    jwt_token = auth_response['access_token']
+    jwt_token = auth_response.json()['access_token']
 
     tenant_response = getTenant(jwt_token)
-    tenant_id = tenant_response['id']
-    data_region = tenant_response['apiHosts']['dataRegion']
+    tenant_id = tenant_response.json()['id']
+    data_region = tenant_response.json()['apiHosts']['dataRegion']
 
     endpoints = getAllEndpoints(jwt_token, tenant_id, data_region)
 
