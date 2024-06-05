206802c4e7d9a3d1ea7c2f290c84388a4531ffaa
Kishiin <47154137+Tesselay@users.noreply.github.com>
2024-06-05 19:26:53 +0200

    Get tenant id and set data region

--- scan.py
+++ scan.py
@@ -1,10 +1,23 @@
 import requests
 
-def main():
-    payload = {'grant_type':'client_credentials', 'client_id':'', 'client_secret':'', 'scope':'token'}
-    header = {'Content-Type':'application/x-www-form-urlencoded'}
+def main(id, secret):
+    auth_response = auth(id, secret)
+    tenant_response = getTenant(auth_response['access_token'])
 
-    r = requests.get('https://id.sophos.com/api/v2/oauth2/token', params=payload, headers=header)
+    data_region = tenant_response['apiHosts']['dataRegion']
+
+def auth(id, secret):
+    payload = {'grant_type':'client_credentials', 'client_id':id, 'client_secret':secret, 'scope':'token'}
+    headers = {'Content-Type':'application/x-www-form-urlencoded'}
+
+    response = requests.post('https://id.sophos.com/api/v2/oauth2/token', params=payload, headers=headers)
+    return response
+
+def getTenant(jwt):
+    headers = {'Authorization: Bearer {}'.format(jwt)}
+
+    response = requests.get('https://api.central.sophos.com/whoami/v1', headers=headers)
+    return response
 
 if __name__ == '__main__':
     main()
